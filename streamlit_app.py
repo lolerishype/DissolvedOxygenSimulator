d39719627d54b061d8909ea2a5802d2f822b30d6
lolerishype <112059514+lolerishype@users.noreply.github.com>
2026-05-19 23:18:26 +0000

    Changed curve colours

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,6 +146,26 @@ with col1:
 with col2:
     fig2 = px.line(df, x=t_label, y=["OTR (mM/h)", "OUR (mM/h)"], title="Oxygen Rates Over Time", labels={"value": "Rate (mM/h)", "variable": "Oxygen Rate"})
     
+    fig2.add_trace(
+        dict(
+            type="scatter",
+            x=sol.t,
+            y=OTR_t,
+            name="OTR",
+            line=dict(color="green")
+        )
+    )
+
+    fig2.add_trace(
+        dict(
+            type="scatter",
+            x=sol.t,
+            y=OUR_t,
+            name="OTR",
+            line=dict(color="red")
+        )
+    )
+    
     fig2.update_layout(
         title_font_size=20,
         title_x=0.5, 
